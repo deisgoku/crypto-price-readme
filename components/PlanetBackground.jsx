6cf42cba30044c2d3d3150ddacb3fbb0fa1a9468
deisgoku <dediiskandar425@gmail.com>
2025-04-21 16:56:11 +0700

    Update PlanetBackground.jsx bantuin Ui

--- components/PlanetBackground.jsx
+++ components/PlanetBackground.jsx
@@ -1,26 +1,20 @@
-// components/PlanetBackground.jsx
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, useGLTF, Stars, Preload } from '@react-three/drei'
-import { Suspense } from 'react'
-
-function Planet() {
-  const { scene } = useGLTF('/planet/scene.gltf')
-  return <primitive object={scene} scale={0.5} position={[0, -1, 0]} />
-}
+'use client';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { useRef } from 'react';
 
 export default function PlanetBackground() {
+  const { scene } = useGLTF('/planet/scene.gltf');
+  const canvasRef = useRef();
+
   return (
-    <div className='absolute inset-0 z-[-1]'>
-      <Canvas camera={{ position: [0, 0, 3] }}>
-        <ambientLight intensity={0.8} />
-        <directionalLight position={[5, 5, 5]} />
-        <Suspense fallback={null}>
-          <Stars radius={100} depth={50} count={5000} factor={4} fade />
-          <Planet />
-        </Suspense>
-        <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={0.5} />
-        <Preload all />
+    <div className="fixed top-0 left-0 w-full h-full z-0">
+      <Canvas ref={canvasRef} camera={{ position: [0, 0, 10], fov: 35 }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} />
+        <primitive object={scene} scale={2} />
+        <OrbitControls enableZoom={false} enablePan={false} />
       </Canvas>
     </div>
-  )
+  );
 }
