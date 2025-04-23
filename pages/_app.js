b8bd132fff48da8729204b6dbca9ce60c453f80d
deisgoku <dediiskandar425@gmail.com>
2025-04-23 09:53:07 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/global.css'
+import "../styles/global.css";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster position="top-center" />
+    </>
+  );
 }
