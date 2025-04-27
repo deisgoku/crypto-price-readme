a07fd55696e557de9be3775d2df430ee5a4fa5f5
deisgoku <dediiskandar425@gmail.com>
2025-04-28 01:58:54 +0700

    next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+
+// next.config.js
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/card',
+        destination: '/api/card',
+      },
+      {
+        source: '/cards',
+        destination: '/api/cards',
+      },
+      {
+        source: '/prices,
+        destination: '/api/prices',
+      },
+    ]
+  },
+}
