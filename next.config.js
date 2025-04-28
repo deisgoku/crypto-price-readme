58bdefce7cb8fcc1448c0ae0bba498e207bcb2ce
deisgoku <dediiskandar425@gmail.com>
2025-04-28 19:07:23 +0700

    Update next.config.js advanced deploy

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
 // next.config.js
-module.exports = {
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  productionBrowserSourceMaps: false,
   async rewrites() {
     return [
-      {
-        source: '/card',
-        destination: '/api/card',
-      },
-      {
-        source: '/cards',
-        destination: '/api/cards',
-      },
-      {
-        source: '/prices',
-        destination: '/api/prices',
-      },
+      { source: '/card', destination: '/api/card' },
+      { source: '/cards', destination: '/api/cards' },
+      { source: '/prices', destination: '/api/prices' },
     ]
   },
 }
+
+module.exports = nextConfig
