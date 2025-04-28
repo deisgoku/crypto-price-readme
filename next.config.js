d920215be780fbf144f2d240dedd3f2e556a66ab
deisgoku <dediiskandar425@gmail.com>
2025-04-28 19:17:10 +0700

    Update next.config.js advance mode

--- next.config.js
+++ next.config.js
@@ -3,6 +3,30 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   productionBrowserSourceMaps: false,
+  
+  images: {
+    unoptimized: true, 
+  },
+
+  experimental: {
+    optimizeCss: true, // CSS optimize
+    scrollRestoration: true, // Scroll back/forward advanved
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', 
+          },
+        ],
+      },
+    ]
+  },
+
   async rewrites() {
     return [
       { source: '/card', destination: '/api/card' },
