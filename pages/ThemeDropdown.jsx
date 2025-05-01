b81d34fe48da770eda35eb0d86219088bc6a819d
deisgoku <dediiskandar425@gmail.com>
2025-05-02 01:19:49 +0700

    Update ThemeDropdown.jsx

--- pages/ThemeDropdown.jsx
+++ pages/ThemeDropdown.jsx
@@ -1,5 +1,5 @@
 import Select from "react-select";
-import { themes } from "../lib/settings/theme";
+import { themes } from "../lib/settings/model/theme";
 
 const themeOptions = Object.keys(themes).map((key) => ({
   value: key,
