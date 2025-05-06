57971dc3579d36e3550be95826b51bf520327647
deisgoku <dediiskandar425@gmail.com>
2025-05-06 22:56:03 +0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteUrl: 'https://crypto-price-on.vercel.app',
   generateRobotsTxt: true,  
-  output: 'export',  
+  output: 'public',  
   sitemapSize: 7000,
   changefreq: 'weekly',
   priority: 0.7,
