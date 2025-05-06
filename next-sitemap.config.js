ed06a94741af7a510f19d95d4df541bbc0ad0101
deisgoku <dediiskandar425@gmail.com>
2025-05-07 00:35:29 +0700

    Update next-sitemap.config.js tes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,7 @@ module.exports = {
   sitemapSize: 7000,
   changefreq: 'weekly',
   priority: 0.7,
-  generateIndexSitemap: true,
+  generateIndexSitemap: false,
   exclude: [
     '/', // homepage
     '/CategoryDropdown',
