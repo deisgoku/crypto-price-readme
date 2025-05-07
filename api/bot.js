2a724158fe46cd5bbb56e31fcbb250fd509ed849
deisgoku <dediiskandar425@gmail.com>
2025-05-08 02:29:23 +0700

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -8,11 +8,26 @@ module.exports = {
     },
   },
 
-  default: function handler(req, res) {
-    if (req.method === 'POST') {
-      bot.handleUpdate(req.body, res);
-    } else {
+  default: async function handler(req, res) {
+    if (req.method !== 'POST') {
       res.status(405).send('Method Not Allowed');
+      return;
     }
+
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+
+    req.on('end', async () => {
+      try {
+        const update = JSON.parse(body);
+        await bot.handleUpdate(update);
+        res.status(200).end();
+      } catch (err) {
+        console.error('Error handling update:', err);
+        res.status(500).send('Internal Server Error');
+      }
+    });
   },
 };
